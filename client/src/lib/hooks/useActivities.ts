d54ea1ca3e4b89300085279c1bffc4c638cc44fe
Slamet Mahendra <BSI90822@bsi.co.id>
2025-06-04 10:24:16 +0700

    End of section 7

--- client/src/lib/hooks/useActivities.ts
+++ client/src/lib/hooks/useActivities.ts
@@ -1,7 +1,7 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import agent from "../api/agent";
 
-export const useActivities = () => {
+export const useActivities = (id?: string) => {
     const queryClient = useQueryClient();
 
     const { data: activities, isPending } = useQuery({
@@ -12,9 +12,19 @@ export const useActivities = () => {
         }
     })
 
+    const {data: activity, isLoading: isLoadingActivity} = useQuery({
+        queryKey: ['activities', id],
+        queryFn: async () => {
+            const response = await agent.get<Activity>(`/activities/${id}`);
+            return response.data;
+        },
+        enabled: !!id // Only run this query if id is defined
+    })
+
     const createActivity = useMutation({
         mutationFn: async (activity: Activity) => {
-            await agent.post('/activities', activity);
+            const response = await agent.post('/activities', activity);
+            return response.data;
         },
         onSuccess: async () => {
             await queryClient.invalidateQueries({
@@ -50,6 +60,8 @@ export const useActivities = () => {
         isPending,
         createActivity,
         updateActivity,
-        deleteActivity
+        deleteActivity,
+        activity,
+        isLoadingActivity
     };
 }
