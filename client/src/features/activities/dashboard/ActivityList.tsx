df17dea3aa39fa024e8539478090177b1ae3a3bf
Slamet Mahendra <BSI90822@bsi.co.id>
2025-06-05 10:47:12 +0700

    End of section 8

--- client/src/features/activities/dashboard/ActivityList.tsx
+++ client/src/features/activities/dashboard/ActivityList.tsx
@@ -3,15 +3,15 @@ import ActivityCard from "./ActivityCard";
 import { useActivities } from "../../../lib/hooks/useActivities";
 
 export default function ActivityList() {
-        const { activities, isPending } = useActivities();
+    const { activities, isPending } = useActivities();
 
     if (!activities || isPending) return <Typography>Loading...</Typography>;
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
             {activities.map((activity) => (
-                <ActivityCard 
-                    key={activity.id} 
-                    activity={activity} 
+                <ActivityCard
+                    key={activity.id}
+                    activity={activity}
                 />
             ))}
         </Box>
